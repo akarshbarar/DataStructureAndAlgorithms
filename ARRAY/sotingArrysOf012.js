e29b4a69c025730b0e5dfa5651c3a3a0bc8d32d4
Akarsh Barar <barar.akarsh8@gmail.com>
2021-05-14 22:21:55 +0530

    Sorting Array of 0 1 2

---
+++ ARRAY/sotingArrysOf012.js
@@ -0,0 +1,19 @@
+var arr=[0,1,2,0,1,2,0,1,2];
+var l=arr.length
+var x=new Array(l).fill(0);
+var y=[]
+for(var i=0;i<l;i++){
+    x[arr[i]]=x[arr[i]]+1;
+}
+
+console.log(x)
+
+for(i=0;i<l;i++){
+   if(x[i] !=0){
+       y.push(i)
+       x[i]=x[i]-1;
+       i--
+   }
+    
+}
+console.log(y)
